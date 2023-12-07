6cd66efd5d100c246d8c2374b84287a5c45d63d4
Qibaoooo <luozhengiw@gmail.com>
2023-12-07 11:17:58 +0800

    created spring project

---
+++ src/main/java/sg/nus/iss/team11/LapsApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/team11/LapsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.team11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
