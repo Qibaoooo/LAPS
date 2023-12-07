0f1f12e4019f578803262959f9cdd2c70e5e1e34
Qibaoooo <luozhengiw@gmail.com>
2023-12-07 18:18:49 +0800

    add ProfessionalEmployee

--- src/main/java/sg/nus/iss/team11/model/Staff.java
+++ src/main/java/sg/nus/iss/team11/model/Staff.java
@@ -4,17 +4,18 @@ import jakarta.persistence.*;
 
 @Entity
 @DiscriminatorValue("Staff")
-public class Staff extends User {
+public class Staff extends ProfessionalEmployee {
 	
 //	TODO: how to implement self-referencing OneToOne mapping here?
 	private int managerId;
-	
+
 	public Staff() {
 		super();
 	}
 
-	public Staff(String username, String password, int managerId) {
-		super(username, password);
+	public Staff(String username, String password, String leaveApplication, int managerId) {
+		super(username, password, leaveApplication);
 		this.managerId = managerId;
-	}
+	}	
+
 }
