233cb3e2efc168a397ce1755a3a95eb88401cdc3
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-12-11 09:43:13 +0800

    history page

--- src/main/java/sg/nus/iss/team11/repository/LeaveApplicationRepository.java
+++ src/main/java/sg/nus/iss/team11/repository/LeaveApplicationRepository.java
@@ -9,6 +9,9 @@ import org.springframework.data.repository.query.Param;
 import sg.nus.iss.team11.model.LeaveApplication;
 
 public interface LeaveApplicationRepository extends JpaRepository<LeaveApplication, Integer> {
-	@Query("SELECT a from LeaveApplication a WHERE a.user.id = :userId")
-	  List<LeaveApplication> findLeaveApplicationsByUserId(@Param("userId") Integer userId);
+	@Query("SELECT la from LeaveApplication la WHERE la.user.id = :userId")
+	List<LeaveApplication> findLeaveApplicationsByUserId(@Param("userId") Integer userId);
+
+	@Query("SELECT la from LeaveApplication la WHERE la.user.id = :userId AND la.status IN ('UPDATED', 'APPLIED')")
+	List<LeaveApplication> findLeaveApplicationsToProcess(@Param("userId") Integer userId);
 }
