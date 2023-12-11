7fc63bf5254cf27a8ec18ec661c74fcaf1b55d31
Qibaoooo <luozhengiw@gmail.com>
2023-12-11 19:49:24 +0800

    minor fixes for CheckLoginInterceptor

--- src/main/java/sg/nus/iss/team11/interceptor/CheckLoginInterceptor.java
+++ src/main/java/sg/nus/iss/team11/interceptor/CheckLoginInterceptor.java
@@ -12,24 +12,31 @@ import jakarta.servlet.http.HttpSession;
 
 @Component
 public class CheckLoginInterceptor implements HandlerInterceptor{
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws IOException {
 		System.out.println("Intercepting: " + request.getRequestURI());
 		HttpSession session = request.getSession();
 		// Check if the user already has set attribute userLogin
-		if (session.getAttribute("username") != null)
+		if (session.getAttribute("user") != null)
 			return true;
 		String[] splitURI = request.getRequestURI().split("/");
+		
 		if (splitURI[splitURI.length - 1].equals("login"))
 			return true;
+		if (splitURI[splitURI.length - 1].equals("authenticate"))
+			return true;
+		
 		// If the user has not logged in, redirect her/him to login
 		response.sendRedirect("/login");
 		return false;
 	}
+	
 	@Override
 	public void postHandle(HttpServletRequest request,
 	HttpServletResponse response, Object handler, ModelAndView modelAndView) {
 	}
+	
 	@Override
 	public void afterCompletion(HttpServletRequest request,
 	HttpServletResponse response, Object handler, Exception ex) {
