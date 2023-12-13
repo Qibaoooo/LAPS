2941565b3337aa8e21d21b1961f55e126af8da1f
HMM020223 <153045845+HMMMM2002@users.noreply.github.com>
2023-12-14 00:46:25 +0800

    admin/employee(show all employee)

--- src/main/java/sg/nus/iss/team11/controller/AdminController.java
+++ src/main/java/sg/nus/iss/team11/controller/AdminController.java
@@ -1,8 +1,26 @@
 package sg.nus.iss.team11.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import sg.nus.iss.team11.controller.service.UserService;
+import sg.nus.iss.team11.model.User;
 
 @Controller
+@RequestMapping(value="/admin")
 public class AdminController {
-
+@Autowired
+public UserService userservice;
+@RequestMapping(value="/employee")
+public String viewEmployeeList(HttpSession session, Model model) {
+	User currentAdmin=(User)session.getAttribute("user");
+	List<User> employees=userservice.findAllUsers();
+	model.addAttribute("Employees", employees);
+	return "employee-list";
+}
 }
