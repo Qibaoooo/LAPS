d7a019a53f2de527873fc70389f62f9719aa7ff1
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-12-16 13:51:36 +0800

    entitlement methods

--- src/main/java/sg/nus/iss/team11/controller/service/HolidayServiceImpl.java
+++ src/main/java/sg/nus/iss/team11/controller/service/HolidayServiceImpl.java
@@ -5,15 +5,27 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sg.nus.iss.team11.model.LeaveApplication;
 import sg.nus.iss.team11.repository.HolidayRepository;
 
 @Service
-public class HolidayServiceImpl implements HolidayService{
+public class HolidayServiceImpl implements HolidayService {
 
 	@Autowired
 	private HolidayRepository holidayrepository;
-	
+
 	public int getHolidayCount(LocalDate startDate, LocalDate toDate) {
 		return holidayrepository.getHolidayCount(startDate, toDate);
+	}
+
+	public int getEntitlement(LeaveApplication application) {
+		int leaveDays = application.countLeaveDays();
+		if (leaveDays > 14) {
+			return leaveDays;
+		} else {
+			int holiday = getHolidayCount(application.getFromDate(), application.getToDate());
+			int weekend = application.countWeekend();
+			return leaveDays - holiday - weekend;
+		}
 	}
 }
