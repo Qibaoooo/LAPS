0c25facd8c6684277b2a4eee7bd3884deb98357b
Qibaoooo <luozhengiw@gmail.com>
2023-12-18 00:58:37 +0800

    formatting

--- frontend/src/pages/managerLeaveList.js
+++ frontend/src/pages/managerLeaveList.js
@@ -8,64 +8,72 @@ import PageTitle from "./components/pageTitle";
 import MyTable from "./components/myTable";
 
 function ManagerLeaveList() {
-    const [leaveList, setLeaveList] = useState([]);
+  const [leaveList, setLeaveList] = useState([]);
 
-    useEffect(() => {
-        if (getUserinfoFromLocal()) {
-            getLeaveList()
-                .then((response) => response.data)
-                .then((list) => {
-                    console.log(list);
-                    setLeaveList(list);
-                });
-        }
-    }, [])
-    const namelist = leaveList.map((userLeaveArray) => (
-                userLeaveArray[0].username.charAt(0).toUpperCase() 
-                + userLeaveArray[0].username.slice(1)))
-    ;
+  useEffect(() => {
+    if (getUserinfoFromLocal()) {
+      getLeaveList()
+        .then((response) => response.data)
+        .then((list) => {
+          console.log(list);
+          setLeaveList(list);
+        });
+    }
+  }, []);
+  const namelist = leaveList.map(
+    (userLeaveArray) =>
+      userLeaveArray[0].username.charAt(0).toUpperCase() +
+      userLeaveArray[0].username.slice(1)
+  );
+  return (
+    <LoginCheckWrapper>
+      <MyNavBar />
+      <PageTitle title="Subordinates Leave Application List"></PageTitle>
 
-    return (
-        <LoginCheckWrapper>
-            <MyNavBar />
-            <PageTitle title="Subordinates Leave Application List"></PageTitle>
-             
-            {leaveList.map((userLeaveArray,index) => (
-                <MyTable>  
-                                   
-                    <thead>
-                        <tr><td colSpan={8}><b>Leave Application for {namelist[index]}</b></td></tr>
-                        <tr>
-                            <th>ID</th>
-                            <th>From Date</th>
-                            <th>To Date</th>
-                            <th>Type</th>
-                            <th>Description</th>
-                            <th>Status</th>
-                            <th>Process</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userLeaveArray.map((value, index) => (
-                            <tr key={index}>
-                                <td width="10%">{value.id}</td>
-                                <td width="15%">{value.fromDate}</td>
-                                <td width="15%">{value.toDate}</td>
-                                <td width="20%">{value.type}</td>
-                                <td width="20%">{value.description}</td>
-                                <td width="10%"><Badge pill bg="warning">
-                                    {value.status}
-                                    </Badge></td>
-                                <td width="10%"><Button variant="secondary" size="sm">Details</Button></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    <div style={{ marginTop: "20px" }}></div>
-                </MyTable>
+      {leaveList.map((userLeaveArray, index) => (
+        <MyTable>
+          <thead>
+            <tr>
+              <td colSpan={8}>
+                <b>Leave Application for {namelist[index]}</b>
+              </td>
+            </tr>
+            <tr>
+              <th>ID</th>
+              <th>From Date</th>
+              <th>To Date</th>
+              <th>Type</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th>Process</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userLeaveArray.map((value, index) => (
+              <tr key={index}>
+                <td width="10%">{value.id}</td>
+                <td width="15%">{value.fromDate}</td>
+                <td width="15%">{value.toDate}</td>
+                <td width="20%">{value.type}</td>
+                <td width="20%">{value.description}</td>
+                <td width="10%">
+                  <Badge pill bg="warning">
+                    {value.status}
+                  </Badge>
+                </td>
+                <td width="10%">
+                  <Button variant="secondary" size="sm">
+                    Details
+                  </Button>
+                </td>
+              </tr>
             ))}
-
-        </LoginCheckWrapper>
-    );
+          </tbody>
+          <div style={{ marginTop: "20px" }}></div>
+        </MyTable>
+      ))}
+    </LoginCheckWrapper>
+  );
 }
 
 export default ManagerLeaveList;
