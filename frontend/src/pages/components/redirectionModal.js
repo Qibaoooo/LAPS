79d5ff810f1aff3bae9dd2daf51efd3a328a0a88
Qibaoooo <luozhengiw@gmail.com>
2023-12-18 21:26:50 +0800

    now can delete claim

--- frontend/src/pages/components/redirectionModal.js
+++ frontend/src/pages/components/redirectionModal.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Modal, Button } from 'react-bootstrap';
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
-function RedirectionModal({ show, handleButtonClick, headerMsg, buttonMsg }) {
-    return (
-      <>
-        <Modal show={show}>
-          <Modal.Body>{headerMsg}</Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleButtonClick}>
-              {buttonMsg}
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+function RedirectionModal({
+  show,
+  handleButtonClick,
+  headerMsg,
+  buttonMsg,
+  enableCloseButton = false,
+  handleClose = ()=>{}
+}) {
+  return (
+    <>
+      <Modal show={show}>
+        <Modal.Body>{headerMsg}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleButtonClick}>
+            {buttonMsg}
+          </Button>
+          {enableCloseButton && <Button variant="secondary" onClick={handleClose}>Close</Button>}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
-export default RedirectionModal
+export default RedirectionModal;
