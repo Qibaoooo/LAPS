ffe6eb75733af589b8bacc47b78fd97581b7968b
HMM020223 <153045845+HMMMM2002@users.noreply.github.com>
2023-12-19 13:13:04 +0800

    add type field+related lists/pages

--- src/main/java/sg/nus/iss/team11/controller/API/payload/EditEmployee.java
+++ src/main/java/sg/nus/iss/team11/controller/API/payload/EditEmployee.java
@@ -1,13 +1,14 @@
 package sg.nus.iss.team11.controller.API.payload;
 
+import sg.nus.iss.team11.model.EmployeeTypeEnum;
 import sg.nus.iss.team11.model.LAPSUser;
 
 public class EditEmployee extends NewEmployee{
 	private int id;
 	
-	public EditEmployee(String username, String password, String managerName, String roleName, int annualLeaveEntitlement,
+	public EditEmployee(String username, String password, String managerName, String roleName, EmployeeTypeEnum type,int annualLeaveEntitlement,
 			int medicalLeaveEntitlement, int compensationLeaveEntitlement, int id) {
-		super(username, password, managerName, roleName, annualLeaveEntitlement,
+		super(username, password, managerName,type, roleName,annualLeaveEntitlement,
 				medicalLeaveEntitlement, compensationLeaveEntitlement);
 		this.id = id;
 	}
