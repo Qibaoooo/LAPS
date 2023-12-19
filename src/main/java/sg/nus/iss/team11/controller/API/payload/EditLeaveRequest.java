6dff9d2cc10254b80141b770ecc3cf538726aa94
calvinpoh98 <calvinpoh01@gmail.com>
2023-12-19 10:55:15 +0800

    Added API for leave new

--- src/main/java/sg/nus/iss/team11/controller/API/payload/EditLeaveRequest.java
+++ src/main/java/sg/nus/iss/team11/controller/API/payload/EditLeaveRequest.java
@@ -1,5 +1,13 @@
 package sg.nus.iss.team11.controller.API.payload;
 
 public class EditLeaveRequest {
-
+	private int id;
+	
+	public EditLeaveRequest() {
+		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 }
